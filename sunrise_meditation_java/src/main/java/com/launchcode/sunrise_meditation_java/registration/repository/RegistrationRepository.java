79d602b39fdd8b5f5a0de4697e8a41c8dfb4cf83
Viji Bala <viji.1003@gmail.com>
2020-12-17 16:30:59 -0600

    Registration changes and Package restructured

--- sunrise_meditation_java/src/main/java/com/launchcode/sunrise_meditation_java/registration/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.launchcode.sunrise_meditation_java.registration.repository ;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.launchcode.sunrise_meditation_java.registration.model.User;
-
-public interface RegistrationRepository extends JpaRepository<User, Long> {
-
-}
