2a946e49d53abbf51cd8ef06dc46e0afb11aa0d9
Dan <dzhu0513@gmail.com>
2021-01-11 21:59:33 -0600

    add total time in table and user id

--- sunrise_meditation_java/src/main/java/com/launchcode/sunrise_meditation_java/model/Meditation.java
+++ sunrise_meditation_java/src/main/java/com/launchcode/sunrise_meditation_java/model/Meditation.java
@@ -6,10 +6,14 @@ import javax.persistence.*;
 @Table(name = "MEDITATION_LOG")
 public class Meditation {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "USER_ID")
         private Long userId;
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "ID")
+        private Long id;
+
 
         @Column(name = "CREATED_TIMESTAMP")
         private String created_timestamp;
@@ -17,16 +21,21 @@ public class Meditation {
         @Column(name = "TIME_LOG")
         private String time_log;
 
+        @Column(name = "Total_Time")
+        private String total_time;
+
         @Column(name = "END_TIMESTAMP")
         private String end_timestamp;
 
     public Meditation() {
     }
 
-    public Meditation(Long userId, String created_timestamp, String time_log, String end_timestamp) {
+    public Meditation(Long userId, Long id, String created_timestamp, String time_log, String total_time, String end_timestamp) {
         this.userId = userId;
+        this.id = id;
         this.created_timestamp = created_timestamp;
         this.time_log = time_log;
+        this.total_time = total_time;
         this.end_timestamp = end_timestamp;
     }
 
@@ -37,6 +46,11 @@ public class Meditation {
     public void setUserId(Long userId) {
         this.userId = userId;
     }
+
+    public Long getId() {
+        return id;
+    }
+
 
     public String getCreated_timestamp() {
         return created_timestamp;
@@ -54,6 +68,14 @@ public class Meditation {
         this.time_log = time_log;
     }
 
+    public String getTotal_time() {
+        return total_time;
+    }
+
+    public void setTotal_time(String total_time) {
+        this.total_time = total_time;
+    }
+
     public String getEnd_timestamp() {
         return end_timestamp;
     }
@@ -62,11 +84,11 @@ public class Meditation {
         this.end_timestamp = end_timestamp;
     }
 
-
-   /* @Override
+    @Override
     public String toString() {
-        return "Created: " + String.valueOf(this.getCreated_timestamp()) + "\n"
+        return  "UserId: " + String.valueOf(this.getUserId()) + "\n"+
+                "Created: " + String.valueOf(this.getCreated_timestamp()) + "\n"
                 + "End: " + String.valueOf(this.getEnd_timestamp()) + "\n"
                 + "Time Log: " + this.getTime_log() + "\n";
-    }*/
+    }
 }
